90a1002a34191265ae89f4b5bec2144c935fa816
JeremyDaugherty <Jeremy.Daug@gmail.com>
2025-02-14 15:58:15 -0600

    Tiny  bit of work on test.

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,32 @@ pub mod markethistory;
 #[cfg(test)]
 mod tests {
     mod process_tests {
+        mod uses_input_should {
+            use crate::process::{InputTag, Process, ProcessInput};
+
+            #[test]
+            pub fn sort_inputs_correctly() {
+                let mut test = Process::new(0, "Test".to_string(), String::new())
+                    .uses_input(ProcessInput::new(2, 10.0)
+                        .with_tag(InputTag::Consumed))
+                    .uses_input(ProcessInput::new(1, 10.0)
+                        .with_tag(InputTag::Consumed))
+                    .uses_input(ProcessInput::new(0, 10.0)
+                        .with_tag(InputTag::Consumed))
+                    .uses_input(ProcessInput::new(2, 10.0)
+                        .with_tag(InputTag::Used))
+                    .uses_input(ProcessInput::new(1, 10.0)
+                        .with_tag(InputTag::Used))
+                    .uses_input(ProcessInput::new(0, 10.0)
+                        .with_tag(InputTag::Used))
+                    .uses_input(ProcessInput::new(2, 10.0))
+                    .uses_input(ProcessInput::new(1, 10.0))
+                    .uses_input(ProcessInput::new(0, 10.0));
+
+                // test inputs here
+            }
+        }
+
         mod do_process_should {
             use std::collections::HashMap;
 
