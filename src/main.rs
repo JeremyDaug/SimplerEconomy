98141614d1c7f487edea5f29fe6c6f7013891afc
Jeremy <Jeremy.Daug@gmail.com>
2026-03-23 00:20:20 -0500

    Making blank files for our new stuff.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
+pub mod city;
+pub mod region;
+pub mod map;
+pub mod hex;
+pub mod plot;
+pub mod market;
+pub mod pop;
+pub mod species;
+pub mod culture;
+pub mod desire;
+pub mod firm;
+pub mod unit;
+pub mod tech;
+pub mod good;
+pub mod process;
+pub mod institution;
+pub mod state;
 
+fn main() {
 
-
-
-pub fn main() {
 }
--- src/specials.rs
+++
@@ -1,3 +0,0 @@
-pub struct Share {}
-
-pub struct Loan {}
