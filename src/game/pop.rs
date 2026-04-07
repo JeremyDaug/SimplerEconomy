5c3abf6459526b66df3768c3fbdf54a539dcd5f8
JeremyDaugherty <Jeremy.Daug@gmail.com>
2026-04-07 18:32:13 -0500

    Added some stuff to pop.

--- src/game/pop.rs
+++ src/game/pop.rs
@@ -1,4 +1,32 @@
+use std::collections::HashMap;
+
 #[derive(Debug, Clone)]
 pub struct Pop {
+    /// The unique ID of the pop. May drop this or replace it to simplify.
+    /// 
+    /// Should be stored in the market alone. ID may be allowed to be non-unique
+    /// between markets, but unique within a market.
     pub id: usize,
+
+    /// The ID of the pop's job. Each pop only has 1 right now.
+    pub job: usize,
+
+    pub property: HashMap<usize, PRow>,
+}
+
+/// # Property Row
+/// 
+/// A row of data attached to the property of a pop. This is used for fine tuned
+/// property management and consumption details.
+/// 
+/// This currently only shows 
+#[derive(Debug, Clone, Copy)]
+pub struct PRow {
+    /// The total amount owned at the moment.
+    pub quantity: f64,
+    
+    /// The Target amount the pop desires to have after all shopping is complete.
+    pub target: f64,
+    /// The amount that has already been earmarked for the pop's use today.
+    pub reserved: f64,
 }
