071740409438cd8b513d53db718a430a705e861b
Jeremy <Jeremy.Daug@gmail.com>
2026-04-08 01:36:12 -0500

    Working through Desire and Pop basics, minimal functionality still.

--- src/game/desire.rs
+++ src/game/desire.rs
@@ -1,4 +1,40 @@
+
+/// # Desire
+/// 
+/// A Desire is things or groups of things that are desired by a pop.
+/// 
+/// 
 #[derive(Debug, Clone)]
 pub struct Desire {
-    
+    /// Useful Identifier which points back to where this desire comes from.
+    pub source: DesireSource,
+
+    /// The goods beings desired. If of length 1, then it's a specific good,
+    /// if it's multiple, then it's a bucket.
+    pub target: Vec<(usize, f64)>
+}
+
+/// # Desire Source
+/// 
+/// Where is the desire's definition derived from.
+#[derive(Debug, Clone, Copy)]
+pub enum DesireSource {
+    Species(usize),
+    Culture(usize),
+    Class(usize),
+    Religion(usize),
+}
+
+impl DesireSource {
+    /// # Unwrap
+    /// 
+    /// Gets the ID of the Desire Source.
+    pub fn unwrap(&self) -> &usize {
+        match self {
+            DesireSource::Species(id) |
+            DesireSource::Culture(id) |
+            DesireSource::Class(id) |
+            DesireSource::Religion(id) => id,
+        }
+    }
 }
--- src/game/pop.rs
+++ src/game/pop.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+use crate::game::{desire::Desire, household::HouseholdDef};
+
 #[derive(Debug, Clone)]
 pub struct Pop {
     /// The unique ID of the pop. May drop this or replace it to simplify.
@@ -11,7 +13,47 @@ pub struct Pop {
     /// The ID of the pop's job. Each pop only has 1 right now.
     pub job: usize,
 
+    ///  The property and details of the property
     pub property: HashMap<usize, PRow>,
+    
+    /// Desires of a pop, a consolidated and organized for satisfaction calculations.
+    /// 
+    /// Nested Vec of Vecs.
+    /// 0: Basic Needs
+    /// 1: Common Needs
+    /// 2: Luxury Needs
+    pub desires: Vec<Vec<Desire>>,
+
+    /// The working desires of the pop, a flat structure that goes :
+    /// Basic Needs -> Common Needs -> Luxury Needs. 
+    /// 
+    /// If a pop satisfies all of these, Luxury needs will be duplicated and added
+    /// to the end. Repeat until they are unable to satisfy any more, or run out of
+    /// useable trade goods.
+    pub working_desires: Vec<Desire>,
+
+    /// The demographic breakdown of this pop.
+    /// 
+    /// This may be expanded to be a vector of Demographic Rows, to consolidate
+    /// multiple pops of different cultures into one.
+    pub demographics: DemoRow,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct DemoRow {
+    /// The Number of households, floating point as growth storage.
+    pub count: f64,
+    /// The definition of this row's household. This is the sum of the baseline plus
+    /// all other demographic effects.
+    pub household: HouseholdDef,
+    /// The species ID, currently should always be 0, which is default human.
+    pub species: usize,
+    /// The culture ID, 0 means none.
+    pub culture: usize,
+    /// The class ID, 0 means none.
+    pub class: usize,
+    /// The Religious ID, 0 means none.
+    pub religion: usize,
 }
 
 /// # Property Row
@@ -19,7 +61,8 @@ pub struct Pop {
 /// A row of data attached to the property of a pop. This is used for fine tuned
 /// property management and consumption details.
 /// 
-/// This currently only shows 
+/// This currently only contains the current quantity, the amount reserved for 
+/// consumption, and the target they want to reach.
 #[derive(Debug, Clone, Copy)]
 pub struct PRow {
     /// The total amount owned at the moment.
